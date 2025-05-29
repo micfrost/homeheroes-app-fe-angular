7eca4cd0002a2204ca0ef8b1b4ca7d7c7b4f6ded
micfro <michalfrost71@gmail.com>
2025-05-29 21:21:36 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
-  selector: 'app-app',
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [HeaderComponent, FooterComponent],
 })
 export class AppComponent {
-
 }
