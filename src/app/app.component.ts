81f93794de481356c3f71f654ef88c600359d161
micfro <michalfrost71@gmail.com>
2025-05-29 23:04:11 +0200

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,9 @@ import {FooterComponent} from "./footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
   imports: [HeaderComponent, FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
 }
